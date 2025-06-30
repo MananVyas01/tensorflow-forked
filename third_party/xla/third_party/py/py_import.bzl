hexagon-geo-surv/tensorflow

Refactor `py_import` macros to avoid unpacking pypi wheels twice.
Instead, the macros and underlying rule copies all the files belonging to `:pkg` target into the output dir.

Action wall time from JSON profiler tracing results:

Action   | Old implementation | New implementation
:------: | :----------------: | :----------------:
jaxlib/tools/jax_cuda_pjrt_py_import_unpacked_wheel   | 73,309.311 ms | 6,249.759 ms
jax_py_import_unpacked_wheel   | 631.268 ms             | 1,442.022 ms
jaxlib/tools/jaxlib_py_import_unpacked_wheel   | 8,807.849 ms       | 3,942.539 ms
jaxlib/tools/jax_cuda_plugin_py_import_unpacked_wheel  | 7,873.122 ms | 7,968.307 ms

PiperOrigin-RevId: 775254690

third_party/xla/third_party/py/py_import.bzl
@@ -5,25 +5,25 @@ load("@rules_python//python:defs.bzl", "py_library")
 def _unpacked_wheel_impl(ctx):
     output_dir = ctx.actions.declare_directory(ctx.label.name)
     wheel = ctx.file.wheel
-    script = """
-    {zipper} x {wheel} -d {output}
-    for wheel_dep in {wheel_deps}; do
-        {zipper} x $wheel_dep -d {output}
-    done
-    """.format(
-        zipper = ctx.executable.zipper.path,
-        wheel = wheel.path,
-        output = output_dir.path,
-        wheel_deps = " ".join([
-            "'%s'" % wheel_dep.path
-            for wheel_dep in ctx.files.wheel_deps
-        ]),
-    )
-    ctx.actions.run_shell(
-        inputs = ctx.files.wheel + ctx.files.wheel_deps,
-        command = script,
+    args = ctx.actions.args()
+    args.add("--wheel=%s" % wheel.path)
+    args.add("--output_dir=%s" % output_dir.path)
+    srcs = [wheel]
+    for d in ctx.attr.wheel_deps:
+        for f in d[DefaultInfo].default_runfiles.files.to_list():
+            srcs.append(f)
+            args.add("--wheel_files=%s" % (f.path))
+    for z in ctx.files.zip_deps:
+        srcs.append(z)
+        args.add("--zip_files=%s" % (z.path))
+    args.set_param_file_format("flag_per_line")
+    args.use_param_file("@%s", use_always = False)
+    ctx.actions.run(
+        arguments = [args],
+        inputs = srcs,
         outputs = [output_dir],
-        tools = [ctx.executable.zipper],
+        executable = ctx.executable.create_py_import_output_tool,
+        mnemonic = "CreatePyImportOutput",
     )
 
     return [
@@ -34,12 +34,13 @@ _unpacked_wheel = rule(
     implementation = _unpacked_wheel_impl,
     attrs = {
         "wheel": attr.label(mandatory = True, allow_single_file = True),
-        "zipper": attr.label(
-            default = Label("@bazel_tools//tools/zip:zipper"),
-            cfg = "exec",
+        "create_py_import_output_tool": attr.label(
+            default = Label("//third_party/py:create_py_import_output_tool"),
             executable = True,
+            cfg = "exec",
         ),
         "wheel_deps": attr.label_list(allow_files = True),
+        "zip_deps": attr.label_list(allow_files = True),
     },
 )
 
@@ -47,12 +48,14 @@ def py_import(
         name,
         wheel,
         deps = [],
-        wheel_deps = []):
+        wheel_deps = [],
+        zip_deps = []):
     unpacked_wheel_name = name + "_unpacked_wheel"
     _unpacked_wheel(
         name = unpacked_wheel_name,
         wheel = wheel,
         wheel_deps = wheel_deps,
+        zip_deps = zip_deps,
     )
     py_library(
         name = name,
@@ -68,4 +71,6 @@ Args:
   deps: dependencies of the py_library.
   wheel_deps: additional wheels to unpack. These wheels will be unpacked in the
               same folder as the wheel.
+  zip_deps: additional zip files to unpack. These files will be extracted
+              in the same folder as the wheel.
 """  # buildifier: disable=no-effect

MananVyas01/tensorflow-forked

third_party/xla/third_party/py/py_import.bzl
@@ -5,25 +5,25 @@ load("@rules_python//python:defs.bzl", "py_library")
 def _unpacked_wheel_impl(ctx):
     output_dir = ctx.actions.declare_directory(ctx.label.name)
     wheel = ctx.file.wheel
-    script = """
-    {zipper} x {wheel} -d {output}
-    for wheel_dep in {wheel_deps}; do
-        {zipper} x $wheel_dep -d {output}
-    done
-    """.format(
-        zipper = ctx.executable.zipper.path,
-        wheel = wheel.path,
-        output = output_dir.path,
-        wheel_deps = " ".join([
-            "'%s'" % wheel_dep.path
-            for wheel_dep in ctx.files.wheel_deps
-        ]),
-    )
-    ctx.actions.run_shell(
-        inputs = ctx.files.wheel + ctx.files.wheel_deps,
-        command = script,
+    args = ctx.actions.args()
+    args.add("--wheel=%s" % wheel.path)
+    args.add("--output_dir=%s" % output_dir.path)
+    srcs = [wheel]
+    for d in ctx.attr.wheel_deps:
+        for f in d[DefaultInfo].default_runfiles.files.to_list():
+            srcs.append(f)
+            args.add("--wheel_files=%s" % (f.path))
+    for z in ctx.files.zip_deps:
+        srcs.append(z)
+        args.add("--zip_files=%s" % (z.path))
+    args.set_param_file_format("flag_per_line")
+    args.use_param_file("@%s", use_always = False)
+    ctx.actions.run(
+        arguments = [args],
+        inputs = srcs,
         outputs = [output_dir],
-        tools = [ctx.executable.zipper],
+        executable = ctx.executable.unpack_wheel_and_unzip_archive_files,
+        mnemonic = "UnpackWheelAndUnzipArchiveFiles",
     )
 
     return [
@@ -34,12 +34,13 @@ _unpacked_wheel = rule(
     implementation = _unpacked_wheel_impl,
     attrs = {
         "wheel": attr.label(mandatory = True, allow_single_file = True),
-        "zipper": attr.label(
-            default = Label("@bazel_tools//tools/zip:zipper"),
-            cfg = "exec",
+        "unpack_wheel_and_unzip_archive_files": attr.label(
+            default = Label("//third_party/py:unpack_wheel_and_unzip_archive_files"),
             executable = True,
+            cfg = "exec",
         ),
         "wheel_deps": attr.label_list(allow_files = True),
+        "zip_deps": attr.label_list(allow_files = True),
     },
 )
 
@@ -47,12 +48,14 @@ def py_import(
         name,
         wheel,
         deps = [],
-        wheel_deps = []):
+        wheel_deps = [],
+        zip_deps = []):
     unpacked_wheel_name = name + "_unpacked_wheel"
     _unpacked_wheel(
         name = unpacked_wheel_name,
         wheel = wheel,
         wheel_deps = wheel_deps,
+        zip_deps = zip_deps,
     )
     py_library(
         name = name,
@@ -68,4 +71,6 @@ Args:
   deps: dependencies of the py_library.
   wheel_deps: additional wheels to unpack. These wheels will be unpacked in the
               same folder as the wheel.
+  zip_deps: additional zip files to unpack. These files will be extracted
+              in the same folder as the wheel.
 """  # buildifier: disable=no-effect